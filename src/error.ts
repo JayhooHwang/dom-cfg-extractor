caeb0144151b2affba5b2467f90c92ece8d7a406
Jayhoo <hellohsq@163.com>
2026-09-10 15:00:32 +0800

    增强 error 模块

--- src/error.ts
+++ src/error.ts
@@ -1,8 +1,41 @@
+type ErrorOccur = {key?:string,item?:any}
+
+const PREFIX = "[DOM_EXTRACTOR_ERR]"
+
 export class DomExtractorError extends Error {
-    constructor(message: string) {
-        super(`[Dom-Cfg-Extractor] ${message}`);
-        this.name = 'DomExtractorError'; 
-        
+    name = "DomExtractorError";
+    occur?:ErrorOccur=undefined
+
+    constructor(message:string, cause?:Error, occur?:ErrorOccur){
+        super(`${PREFIX} ${getErrorMessage(message, occur)}`, {cause});
+        this.occur = occur;
+        //处理 stack —— 用 this.stack 的第一行，替换 cause.stack 的第一行，赋值回 this.stack
+        if(cause && cause.stack){
+            const [, ...causeStacks] = cause.stack.split("\n");
+            const thisStacks = this.stack?.split("\n") || [];
+            this.stack = [thisStacks[0], ...causeStacks].join("\n");
+        }
+
         Object.setPrototypeOf(this, DomExtractorError.prototype);
+    }
+}
+
+export function consoleWarning(message:string, ex?:Error|unknown){
+    console.warn(`${PREFIX} ${message}`, ex)
+}
+
+function getErrorMessage(message:string, occur?:ErrorOccur){
+    let msg = message;
+    if(occur){
+        msg += "(occur key=" + occur?.key + ")";
+    }
+    return msg;
+}
+
+export function resolveCatch(ex:unknown, occur?:ErrorOccur){
+    if(ex instanceof Error){
+        throw new DomExtractorError(ex.message, ex, occur);
+    }else{
+        throw new DomExtractorError(String(ex), undefined, occur);
     }
 }
